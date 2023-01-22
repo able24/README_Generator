6690b79327526ae3146f0e326e34781576a3196a
Abel <egbeleabel@yahoo.com>
2023-01-22 17:17:14 +0000

    Added questions with inquirer

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## ${data.description}
+  ## ${data.tableOfContent}
+  ## ${data.installation}
+  ## ${data.usage}
+  ## ${data.license}
+  ## ${data.contribution}
+  ## ${data.tests}
+  ## ${data.questions}
 
 `;
 }

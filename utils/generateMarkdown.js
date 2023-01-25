eb89dc2acd0fde5d4b8dc9df49db2ef07efec177
Abel <egbeleabel@yahoo.com>
2023-01-25 14:10:21 +0000

    modularization finished

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,40 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-  ## ${data.description}
-  ## ${data.tableOfContent}
-  ## ${data.installation}
-  ## ${data.usage}
-  ## ${data.license}
-  ## ${data.contribution}
-  ## ${data.tests}
-  ## ${data.questions}
 
-`;
-}
+function generateMarkdown(data) {
+  return `
+# ${data.title}
+
+## Table of Content
+- [Project description](#description)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Installation](#installation)
+- [Test Instructions](#test instructions)
+- [Questions](#questions)
+- [License](#license)
+
+## Description
+${data.description}
+
+## Usage
+${data.usage}
+
+## Installation
+${data.installation}
+
+## Contributing
+${data.contributing}
+
+## Questions
+${data.email}
+${data.github}
+
+## Test Instructions
+${data.test}
+
+## License
+${data.license}
+`
+};
 
 module.exports = generateMarkdown;
